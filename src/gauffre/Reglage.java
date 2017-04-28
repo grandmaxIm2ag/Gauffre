da529c0be90ee235d002f290665a978fb86d485f
%axence Grand <maxencegrand1995@gmail.com>
2017-04-28 15:40:38 +0200

    Merge IA into moteur
    Conflicts:
    	src/Modele/Arbitre.java
    	src/gauffre/Gauffre.java

---
+++ src/gauffre/Reglage.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gauffre;
+
+import java.util.NoSuchElementException;
+import java.util.Properties;
+
+public class Reglage {
+    static Properties prop;
+
+    public static void init(Properties p) {
+        System.err.println("Partie en "+p.getProperty("nbManche"));
+        prop = p;
+    }
+    
+    public static float lis(String nom) {
+        String value = prop.getProperty(nom);
+        if (value != null)
+            return Integer.parseInt(value);
+        else
+            throw new NoSuchElementException("Propriété "+nom+" manquante");
+    }
+}
