41660c8b8b2e471d8ee92351ea4b94c51c094a4c
%axence Grand <maxencegrand1995@gmail.com>
2017-04-28 17:03:14 +0200

    Merge IA into moteur
    Conflicts:
    	src/Modele/Arbitre.java
    	src/gauffre/Gauffre.java

---
+++ src/Controlleur/Bouton.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlleur;
+
+import Modele.Arbitre;
+import Vue.Interface;
+import javafx.event.*;
+
+/**
+ *
+ * @author grandmax
+ */
+public class Bouton implements EventHandler<ActionEvent>{
+    int value;
+    Arbitre ab;
+    
+    public Bouton(int v, Arbitre a){
+        value = v;
+        ab = a;
+    }
+    
+    @Override
+    public void handle(ActionEvent t) {
+        /*switch(value){
+            case Interface.BOUTTON_ANNULER:
+                ab.precedent();
+                break;
+            case Interface.BOUTTON_REFAIRE:
+                ab.refaire();
+                break;
+            case Interface.BOUTTON_NOUVEL:
+                ab.nouvellePartie();
+                break;
+            case Interface.BOUTTON_SAUVEGARDER:
+                ab.sauvegarder();
+                break;
+        }*/
+    }
+    
+}
--- src/Vue/Interface.java
+++ src/Vue/Interface.java
@@ -5,6 +5,7 @@
  */
 package Vue;
 
+import Controlleur.Bouton;
 import Controlleur.Souris;
 import javafx.application.*;
 import javafx.stage.Stage;
@@ -33,10 +34,18 @@ import javafx.scene.layout.VBox;
  * @author grandmax
  */
 public class Interface extends Application{
+    public final static int BOUTTON_NOUVEL = 0;
+    public final static int BOUTTON_REFAIRE = 1;
+    public final static int BOUTTON_ANNULER = 2;
+    public final static int BOUTTON_SAUVEGARDER = 3;
+     
     static Arbitre arbitre;
 
     @Override
     public void start(Stage stage) throws Exception {
+        
+       
+        
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         stage.setTitle("Gauffre empoisonnee");
         BorderPane b = new BorderPane();
@@ -55,24 +64,19 @@ public class Interface extends Application{
         paneRight.setPadding(new Insets(20));
         paneRight.setSpacing(10);
         paneRight.setAlignment(Pos.TOP_CENTER);
-        Button b1 = new Button("Truc"); 
-        b1.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Faire un truc !");
-            }
-        });
-        Button b2 = new Button("Dire \"Jean\""); 
-        b2.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Jean");
-            }
-        });
+        Button b1 = new Button("Nouvelle Partie");
+        Button b2 = new Button("Précédent");
+        Button b3 = new Button("Refaire");
+        Button b4 = new Button("Sauvegarder");
+        b1.setOnAction(new Bouton(BOUTTON_NOUVEL, arbitre));
+        b1.setOnAction(new Bouton(BOUTTON_ANNULER, arbitre));
+        b1.setOnAction(new Bouton(BOUTTON_REFAIRE, arbitre));
+        b1.setOnAction(new Bouton(BOUTTON_SAUVEGARDER, arbitre));
+        
         paneRight.getChildren().add(b1);
         paneRight.getChildren().add(b2);
+        paneRight.getChildren().add(b3);
+        paneRight.getChildren().add(b4);
         b.setRight(paneRight);
             
         Scene s;
