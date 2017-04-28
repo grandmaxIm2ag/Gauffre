7dcae7684a7cbca2b3002310c507a962ac7edc9a
%axence Grand <maxencegrand1995@gmail.com>
2017-04-28 14:53:38 +0200

    Merge IA into moteur
    Conflicts:
    	src/Modele/Arbitre.java
    	src/gauffre/Gauffre.java

--- src/gauffre/Configuration.java
+++ src/gauffre/Configuration.java
@@ -5,10 +5,35 @@
  */
 package gauffre;
 
+import java.io.*;
+import java.util.*;
+
 /**
  *
  * @author grandmax
  */
 public class Configuration {
     
+    static void erreur(Exception e, String nom) {
+        System.err.println("Impossible de charger " + nom);
+        System.err.println(e);
+        System.exit(1);
+    }
+
+    static void chargerProprietes(Properties p, InputStream in) {
+        try {
+            p.load(in);
+        } catch (IOException e) {
+            erreur(e, "Ressources/defaut.cfg");
+        }
+    }
+
+    public static Properties proprietes() {
+        Properties p;
+        InputStream in = ClassLoader.getSystemClassLoader().getResourceAsStream("Ressources/defaut.cfg");
+        Properties defaut = new Properties();
+        chargerProprietes(defaut, in);
+        p = defaut;
+        return p;
+    }
 }
